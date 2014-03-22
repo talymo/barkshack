a9963cfc779bff2e725079b0d17902fba917df04
talymo <icreatetheinternet@gmail.com>
2014-03-22 00:52:10 +0000

    Added the google map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,49 @@ angular.module('barkApp')
       'AngularJS',
       'Karma'
     ];
+      
+    var styles = [
+        {
+            featureType: 'landscape',
+            elementType: 'all',
+            stylers: [
+                { hue: '#8E887D' },
+                { saturation: -74 },
+                { lightness: -41 },
+                { visibility: 'on' }
+            ]
+        },
+        { 
+            featureType: 'water', 
+            elementType: 'all', 
+            stylers: [ 
+                { hue: '#A1C1BE' }, 
+                { saturation: -54 }, 
+                { lightness: -9 }, 
+                { visibility: 'on' } 
+            ] 
+        }
+    ];
+    var options = {
+        mapTypeControlOptions: {
+            mapTypeIds: [ 'Styled']
+        },
+        center: new google.maps.LatLng(32.2305004,-80.8568941),
+        zoom: 16,
+        mapTypeId: 'Styled'
+    };
+    var div = document.getElementById('map');
+    var map = new google.maps.Map(div, options);
+    var styledMapType = new google.maps.StyledMapType(styles, { name: 'Styled' });
+    map.mapTypes.set('Styled', styledMapType);
+      
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(32.2305004,-80.8568941),
+          map: map,
+          title: 'Barkshack',
+          icon: 'images/marker.png'
+      });
+
+
+      
   });
